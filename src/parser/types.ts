c883cadf4b0f3e98eac437ea6ec0fcf715e58b88
Muronuch <zhigallomaks@gmail.com>
2026-04-17 14:07:32 +0200

    feat(parser): define ComponentDescriptor types

---
+++ src/parser/types.ts
@@ -0,0 +1,39 @@
+// src/parser/types.ts
+export interface PropField {
+  name: string;
+  type: string;
+  optional: boolean;
+}
+
+export interface HookUsage {
+  hook: string;
+  args_summary: string;
+}
+
+export interface JsxTreeNode {
+  tag: string;
+  children_count: number;
+  depth: number;
+  has_map: boolean;
+  has_conditional: boolean;
+}
+
+export type ExportType = "default" | "named";
+
+export interface ComponentDescriptor {
+  file_path: string;
+  component_name: string;
+  export_type: ExportType;
+  props: PropField[];
+  hooks: HookUsage[];
+  jsx_tree: JsxTreeNode[];
+  jsx_depth: number;
+  jsx_element_count: number;
+  has_state: boolean;
+  has_effects: boolean;
+  has_context: boolean;
+  has_refs: boolean;
+  imports: string[];
+  line_count: number;
+  source_code: string;
+}
